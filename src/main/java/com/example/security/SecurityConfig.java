491f6de5b7f3e08d94e61f608f9de97fea653de5
huytusneaker <dhtu12345@gmail.com>
2022-09-07 17:33:24 +0700

    dhtu1 swagger

--- src/main/java/com/example/security/SecurityConfig.java
+++ src/main/java/com/example/security/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -49,10 +50,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v2/api-docs",
+                "/configuration/ui",
+                "/swagger-resources/**",
+                "/configuration/security",
+                "/swagger-ui.html",
+                "/webjars/**");
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.authorizeRequests().antMatchers("/login/**", "/api/login/**", "/api/user/register", "/api/token/refresh/**", "/api/products", "/api/product/**").permitAll();
+        http.authorizeRequests().antMatchers("/login/**", "/api/login/**", "/api/user/register",
+                "/api/token/refresh/**", "/api/products",
+                "/api/product/**"
+        , "/swagger-ui.html", "/html/**", "/resources/**", "/static/**", "/css/**", "/js/**", "/img/**", "/icon/**").permitAll();
         http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/admin/users").hasAnyAuthority("ADMIN");
         http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/admin/user/**").hasAnyAuthority("ADMIN");
         http.authorizeRequests().anyRequest().authenticated()
