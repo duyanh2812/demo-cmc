fec98c6de331f8d16bde624fe94a70176c8200b1
Minh Nguyen Cong <ncminh@cmcglobal.vn>
2022-09-07 11:14:47 +0700

    n commit

--- src/main/java/com/example/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/exception/GlobalExceptionHandler.java
@@ -1,24 +1,32 @@
-//package com.example.exception;
-//
-//import org.springframework.http.HttpHeaders;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.MethodArgumentNotValidException;
-//import org.springframework.web.bind.annotation.ControllerAdvice;
-//import org.springframework.web.bind.annotation.ExceptionHandler;
-//import org.springframework.web.context.request.WebRequest;
-//
-//import java.util.Date;
-//import java.util.LinkedHashMap;
-//import java.util.List;
-//import java.util.Map;
-//import java.util.stream.Collectors;
-//
-//@ControllerAdvice
-//public class GlobalExceptionHandler {
-//    private Map<String, Object> body = new LinkedHashMap<>();
-//
-//    //bắt xử lý ngoại lệ
+package com.example.exception;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    private Map<String, Object> body = new LinkedHashMap<>();
+
+    @ExceptionHandler(UserException.class)
+    public ResponseEntity<?> handleUserException(UserException e) {
+        body.put("Timestamp", new Date());
+        body.put("Status", HttpStatus.BAD_REQUEST.value());
+        body.put("Errors", e.getMessage());
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
+
+    //bắt xử lý ngoại lệ
 //    @ExceptionHandler(NumberFormatException.class)
 //    public ResponseEntity<?> numberFormatException(NumberFormatException e){
 //        body.put("Timestamp", new Date());
@@ -48,4 +56,4 @@
 //        body.put("Errors", errors);
 //        return new ResponseEntity<>(body, headers, status);
 //    }
-//}
+}
